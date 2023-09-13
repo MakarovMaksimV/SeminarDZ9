d7d726ab0611bcc40f098eb24fc1ccda075579ce
Maksim <mirror0791@mail.ru>
2023-09-14 00:49:02 +0300

    Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+System.Console.WriteLine(PrintNumbers(80, 9));
+
+string PrintNumbers(int end, int start)
+{
+    if (end == start)
+    {
+        return end.ToString();
+    }
+    if(end < start)
+    {
+        return "Числа заданы не верно";
+    }
+    else
+    {
+        return end + " " + PrintNumbers(end - 1, start);
+    }
+}
